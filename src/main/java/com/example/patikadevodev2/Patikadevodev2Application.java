50740556cb0c71a2c0ce8c22c2d1c8c931940291
Kubilay Tunçbaş <tuncbaskubilay@gmail.com>
2021-08-19 00:13:15 +0300

    başlandı

---
+++ src/main/java/com/example/patikadevodev2/Patikadevodev2Application.java
@@ -0,0 +1,13 @@
+package com.example.patikadevodev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patikadevodev2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patikadevodev2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/patikadevodev2/Patikadevodev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patikadevodev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patikadevodev2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
